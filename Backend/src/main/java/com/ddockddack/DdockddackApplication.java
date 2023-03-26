6ef322e806e4badf9de60e3d504559a9e7d72ccf
Ohsesol <tpthf1@gmail.com>
2023-03-26 16:30:10 +0900

    Refactor : request parameter logger AOP 적용

--- Backend/src/main/java/com/ddockddack/DdockddackApplication.java
+++ Backend/src/main/java/com/ddockddack/DdockddackApplication.java
@@ -2,11 +2,13 @@ package com.ddockddack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class DdockddackApplication {
 
     static {
